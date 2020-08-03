245214b9e9d03291ca3483e638da2787eb00c892
boseok1 <62227398+boseok1@users.noreply.github.com>
2020-08-03 10:44:43 +0900

    스프링 시큐리티 기본 v1

---
+++ src/main/java/com/cos/securityex01/config/auth/WebMvcConfig.java
@@ -0,0 +1,30 @@
+package com.cos.securityex01.config.auth;
+
+
+
+
+
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		MustacheViewResolver resolver = new MustacheViewResolver();
+		
+		resolver.setCharset("UTF-8");
+        resolver.setContentType("ContentType");
+	    resolver.setPrefix("classpath:/templates");	
+        resolver.setSuffix(".html");
+		
+		
+		registry.viewResolver(resolver);
+					
+		
+	}
+}
